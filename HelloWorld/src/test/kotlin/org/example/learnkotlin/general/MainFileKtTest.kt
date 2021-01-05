14a25e277fd08e343aceeed92f7e31446ca0fe4d
Vlad_Shilov <v.shilov.mobile@gmail.com>
2021-01-05 11:32:05 +0000

    Vsh_20210105

--- HelloWorld/src/test/kotlin/org/example/learnkotlin/general/MainFileKtTest.kt
+++ HelloWorld/src/test/kotlin/org/example/learnkotlin/general/MainFileKtTest.kt
@@ -1,12 +1,14 @@
 package org.example.learnkotlin.general
 
+import java.time.LocalDate
 import kotlin.test.Test
 
 
 class MainFileKtTest {
     @Test
     fun someTest()  {
-
+        val cntr = Contract("123", LocalDate.of(2020,12,25), "OOO Primer")
+        cntr.printContract()
     }
 
 
